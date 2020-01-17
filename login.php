cbd6ee57604db85ff234e84c5bf90dfdd62666ad
Joshua M. Friedman <joshuaf03@icloud.com>
2020-01-17 17:13:29 -0500

    Updated js

--- login.php
+++ login.php
@@ -51,9 +51,9 @@ if(isset($_POST['username'])){
 <html>
 	<head>
         <title>Login - FantasyFRC</title>
-        <link rel="stylesheet" type="text/css" href="fantasy.css?version=9">
+        <link rel="stylesheet" type="text/css" href="fantasy.css?version=10">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<script src="fantasy.js"></script>
+		<script src="fantasy.js?version=10"></script>
 	</head>
 	<body style="background-color:#cccccc">
 		<div id="navbar" class="navbar">
